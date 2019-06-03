5de7044ee348947422dc68eca9f997b1b07b5002
张新永 <xinyong.zhang1@baozun.com>
2019-06-03 13:56:30 +0800

    switch case

---
+++ src/Date0603.java
@@ -0,0 +1,21 @@
+public class Date0603 {
+    public static void main(String[] args) {
+        int i = 9;
+        switch (i) {
+            case 0:
+                System.out.println(i);
+                break;
+            case 2:
+                System.out.println(i);
+                break;
+            case 90:
+                System.out.println(i);
+                break;
+            case 10:
+                System.out.println(i);
+                break;
+            default:
+                System.out.println("不匹配执行");
+        }
+    }
+}
