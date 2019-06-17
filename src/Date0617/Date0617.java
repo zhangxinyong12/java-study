5035741e7ff844c6e82ff4c005fc686ce009fb2f
zhangxinyong12 <13271150671@wo.cn>
2019-06-17 22:32:09 +0800

    包名 public

---
+++ src/Date0617/Date0617.java
@@ -0,0 +1,12 @@
+package Date0617;
+
+public class Date0617 {
+    public static void main(String[] args) {
+        System.out.println("null缺省值");
+        People zhangsan=new People();
+        zhangsan.name="zhangsan";
+        zhangsan.age=23;
+        zhangsan.sex="nan";
+        System.out.println(zhangsan);
+    }
+}
---
+++ src/Date0617/People.java
@@ -0,0 +1,7 @@
+package Date0617;
+
+public class People {
+   public String name;
+   public int age;
+   public String sex;
+}
